adb8384e305c96294b18b24eb53859137a5dc87b
Hakan Bilgi <hknblg34@gmail.com>
2022-10-23 22:32:32 +0300

    finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
+import Header from './components/Header/Header';
+import Main from './components/Main/Main';
+import data from './assests/data';
+import {useState} from 'react';
 
 function App() {
+const [page, setPage] = useState(1);
+const userPerPage = 5;      // her sayfada 5 adet resim olsun
+
+const indexOffLastUser = page * userPerPage;
+const indexOffFirstUser = indexOffLastUser - userPerPage;
+
+const currentUsers = data.slice(indexOffFirstUser, indexOffLastUser);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header from={indexOffFirstUser + 1} to={indexOffFirstUser + currentUsers.length}/>
+      {currentUsers.map((item) => <Main key={item.id}{...item}/>)}
+      <Button total={Math.ceil(data.length / userPerPage)} page={page} setPage={setPage}/>
     </div>
   );
 }
